3410dfed63d0dedb02c7a1b41e73fc612a9b7c3f
mykel <michaeljumoc@gmail.com>
2023-02-16 01:16:19 +0800

    astro config adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    output: 'server',
+    output: 'static',
     adapter: vercel(),
 });
