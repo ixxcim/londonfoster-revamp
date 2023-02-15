e2fd02f6a9690f84dd91ef00a6a866b13f62ce99
mykel <michaeljumoc@gmail.com>
2023-02-16 01:14:05 +0800

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
+import react from '@astrojs/react';
 // https://astro.build/config
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+// https://astro.build/config
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+    integrations: [tailwind(), react()],
+    output: 'server',
+    adapter: vercel(),
 });
