40e89d96953440979b78eb36e1364219ac3efec5
mykel <michaeljumoc@gmail.com>
2023-02-19 02:25:07 +0800

    adjustments

--- src/styles/style.css.ts
+++ src/styles/style.css.ts
@@ -3,3 +3,11 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
     backgroundImage: 'linear-gradient(to right, #002243, #006587)',
 });
+
+export const ImageGrid = style({
+    display: 'grid',
+    gridGap: '15px',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(240px, 1fr))',
+    gridTemplateRows: 'repeat(3, 150px)',
+    gridAutoFlow: 'dense',
+});
